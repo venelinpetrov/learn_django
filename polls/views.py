a86536f8ec599bac437393d412983c07418501e1
venelinpetrov <venelin.petrov@hyperscience.com>
2021-05-26 11:00:32 +0300

    Play with templates

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,22 @@
+from django.http.response import Http404
+from polls.models import Question
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello Django')
+    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_questions_list
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse('You are looking at question id %s' % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse('You are looking at the results of question %s' % question_id)
